2b4192097f656126a2ab402f44a79af0e37b6184
Dima <zahrebelny@gmail.com>
2021-02-06 20:48:16 +0200

    added generate mocks

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const ExitCode = {
+  success: 1,
+};
+
+const DEFAULT_COMMAND = `--version`;
+
+const USER_ARGV_INDEX = 2;
+
+module.exports = {
+  ExitCode,
+  DEFAULT_COMMAND,
+  USER_ARGV_INDEX
+};
---
+++ src/service/cli/generate.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const fs = require(`fs`);
+const {getRandomInt, shuffle} = require(`../../utils`);
+const {ExitCode} = require(`../../constants`);
+
+const DEFAULT_COUNT = 1;
+const FILE_NAME = `mocks.json`;
+
+const TITLES = [
+  `Ёлки. История деревьев`,
+  `Как перестать беспокоиться и начать жить`,
+  `Как достигнуть успеха не вставая с кресла`,
+  `Обзор новейшего смартфона`,
+  `Лучшие рок-музыканты 20-века`,
+  `Как начать программировать`,
+  `Учим HTML и CSS`,
+  `Что такое золотое сечение`,
+  `Как собрать камни бесконечности`,
+  `Борьба с прокрастинацией`,
+  `Рок — это протест`,
+  `Самый лучший музыкальный альбом этого года`,
+];
+
+const ANNOUNCE = [
+  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
+  `Первая большая ёлка была установлена только в 1938 году.`,
+  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
+  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
+  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
+  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
+  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
+  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
+  `Программировать не настолько сложно, как об этом говорят.`,
+  `Простые ежедневные упражнения помогут достичь успеха.`,
+  `Это один из лучших рок-музыкантов.`,
+  `Он написал больше 30 хитов.`,
+  `Из под его пера вышло 8 платиновых альбомов.`,
+  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
+  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
+  `Достичь успеха помогут ежедневные повторения.`,
+  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
+  `Как начать действовать? Для начала просто соберитесь.`,
+  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
+  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
+];
+
+const CATEGORIES = [
+  `Деревья`,
+  `За жизнь`,
+  `Без рамки`,
+  `Разное`,
+  `IT`,
+  `Музыка`,
+  `Кино`,
+  `Программирование`,
+  `Железо`,
+];
+
+const randomTime = () => {
+  const firstDate = new Date().getTime();
+  const lastDate = new Date(new Date() - 1000 * 60 * 60 * 24 * 90).getTime();
+  const diff = lastDate - firstDate;
+  const newDiff = diff * Math.random();
+  const date = new Date(firstDate + newDiff);
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDay() + 1} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+};
+
+const generatePublications = (count) => (
+  Array(count).fill({}).map(() => ({
+    announce: shuffle(ANNOUNCE).slice(1, 5).join(` `),
+    fullText: shuffle(ANNOUNCE).slice(1, 5).join(` `),
+    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    сategory: [ANNOUNCE[getRandomInt(0, CATEGORIES.length - 1)]],
+    createdDate: randomTime(),
+  }))
+);
+
+module.exports = {
+  name: `--generate`,
+  run(args) {
+    const [count] = args;
+    const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
+    const content = JSON.stringify(generatePublications(countOffer));
+    console.log(content);
+    fs.writeFile(FILE_NAME, content, (err) => {
+      if (err) {
+        console.error(`Can't write data to file...`);
+        process.exit(ExitCode.error);
+      }
+
+      return console.info(`Operation success. File created.`);
+    });
+  }
+};
