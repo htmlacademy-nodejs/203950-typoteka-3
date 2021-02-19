ce00e38a4bd6c50728070c156101b329c6190100
Dima <zahrebelny@gmail.com>
2021-02-19 18:54:29 +0200

    added server

--- src/constants.js
+++ src/constants.js
@@ -8,8 +8,17 @@ const DEFAULT_COMMAND = `--version`;
 
 const USER_ARGV_INDEX = 2;
 
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
+
 module.exports = {
   ExitCode,
   DEFAULT_COMMAND,
-  USER_ARGV_INDEX
+  USER_ARGV_INDEX,
+  HttpCode
 };
