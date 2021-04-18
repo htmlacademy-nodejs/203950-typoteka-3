222a8a9284c72e57572c09954f07dbd3da09c93f
Dima <zahrebelny@gmail.com>
2021-04-18 22:03:00 +0300

    connected data from server to templates

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -2,10 +2,70 @@
 
 const {Router} = require(`express`);
 const articlesRouter = new Router();
+const multer = require(`multer`);
+const path = require(`path`);
+const {nanoid} = require(`nanoid`);
+
+const {getAPI} = require(`../api`);
+
+const api = getAPI();
+const UPLOAD_DIR = `../upload/img/`;
+const uploadDirAbsolute = path.resolve(__dirname, UPLOAD_DIR);
+
+const storage = multer.diskStorage({
+  destination: uploadDirAbsolute,
+  filename: (req, file, cb) => {
+    const uniqueName = nanoid(10);
+    const extension = file.originalname.split(`.`).pop();
+    cb(null, `${uniqueName}.${extension}`);
+  }
+});
+
+const upload = multer({storage});
 
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles/articles-by-category`));
-articlesRouter.get(`/add`, (req, res) => res.render(`articles/new-post`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.render(`articles/new-post`));
+articlesRouter.get(`/add`, async (req, res) => {
+  try {
+    const categories = await api.getCategories();
+    res.render(`articles/new-post`, {categories});
+  } catch (error) {
+    res.render(`articles/new-post`, {categories: []});
+  }
+});
+
+articlesRouter.post(`/add`, upload.single(`upload`), async (req, res) => {
+  const {body, file} = req;
+
+  const offerData = {
+    photo: file.filename,
+    announce: body.announcement,
+    fullText: body[`full-text`],
+    title: body.title,
+    category: Array.isArray(body.category) ? body.category : [body.category],
+  };
+
+  try {
+    await api.createArticle(offerData);
+    res.redirect(`/my`);
+  } catch (error) {
+    res.redirect(`back`);
+  }
+});
+
+articlesRouter.get(`/edit/:id`, async (req, res) => {
+  try {
+    const {id} = req.params;
+    const [article, categories] = await Promise.all([
+      api.getArticle(id),
+      api.getCategories()
+    ]);
+
+    res.render(`articles/new-post`, {article, categories});
+  } catch (error) {
+    res.render(`articles/new-post`, {article: [], categories: []});
+  }
+});
+
 articlesRouter.get(`/:id`, (req, res) => res.render(`articles/post`));
 
 module.exports = articlesRouter;
--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -3,10 +3,48 @@
 const {Router} = require(`express`);
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.render(`main`));
+const {getAPI} = require(`../api`);
+
+const api = getAPI();
+
+mainRouter.get(`/`, async (req, res) => {
+  try {
+    const articles = await api.getArticles();
+
+    const popularArticles = articles.slice(0, 5);
+    const lastComments = articles
+      .reduce((acc, article) => [...acc, ...article.comments], [])
+      .slice(0, 3);
+
+    res.render(`main`, {popularArticles, lastComments, articles});
+  } catch (error) {
+    res.render(`main`, {popularArticles: [], lastComments: [], articles: []});
+  }
+});
+
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
-mainRouter.get(`/search`, (req, res) => res.render(`search`));
-mainRouter.get(`/categories`, (req, res) => res.render(`all-categories`));
+
+mainRouter.get(`/search`, async (req, res) => {
+  try {
+    const {search} = req.query;
+
+    const results = await api.search(search);
+    res.render(`search`, {results});
+
+  } catch (error) {
+    res.render(`search`, {results: []});
+  }
+});
+
+mainRouter.get(`/categories`, async (req, res) => {
+  try {
+    const categories = await api.getCategories();
+
+    res.render(`all-categories`, {categories});
+  } catch (error) {
+    res.render(`all-categories`, {categories: []});
+  }
+});
 
 module.exports = mainRouter;
